224bfe0868ccb51d5f9bef188c90160aee381ad0
yutian.liu <939029835@qq.com>
2021-05-30 06:57:42 +0800

    新增对cpp的网络请求支持

--- JsSource/openfl/events/EventDispatcher.hx
+++ JsSource/openfl/events/EventDispatcher.hx
@@ -1,38 +1,40 @@
 package openfl.events;
 
 class EventDispatcher {
-    
-    public function new() {
-        
-    }
+	public var eventsBinds:Map<String, Array<Dynamic->Void>> = [];
 
-    public function addEventListener<T>(type:String, listener:T->Void, useCapture:Bool = false, priority:Int = 0, useWeakReference:Bool = false):Void {
-		// if (__listener == null)
-		// 	__listener = new EventDispatcher(this);
-		// __listener.addEventListener(type, listener, useCapture, priority, useWeakReference);
+	public function new() {}
+
+	public function addEventListener<T>(type:String, listener:T->Void, useCapture:Bool = false, priority:Int = 0, useWeakReference:Bool = false):Void {
+		if (!eventsBinds.exists(type)) {
+			eventsBinds.set(type, []);
+		}
+		eventsBinds.get(type).push(listener);
 	}
 
 	public function removeEventListener<T>(type:String, listener:T->Void, useCapture:Bool = false):Void {
-		// if (__listener != null)
-		// 	__listener.removeEventListener(type, listener, useCapture);
+		if (eventsBinds.exists(type)) {
+			eventsBinds.get(type).remove(listener);
+		}
 	}
 
 	public function dispatchEvent(event:Event):Bool {
-		// if (__listener != null)
-		// 	return __listener.dispatchEvent(event);
+		if (!eventsBinds.exists(event.type))
+			return false;
+		for (call in eventsBinds.get(event.type)) {
+			call(event);
+		}
 		return false;
 	}
 
 	public function hasEventListener(type:String):Bool {
-		// if (__listener != null)
-		// 	return __listener.hasEventListener(type);
+		if (eventsBinds.exists(type)) {
+			return eventsBinds.get(type).length > 0;
+		}
 		return false;
 	}
 
 	public function willTrigger(type:String):Bool {
-		// if (__listener != null)
-		// 	return __listener.willTrigger(type);
 		return false;
 	}
-
 }
--- JsSource/openfl/net/URLLoader.hx
+++ JsSource/openfl/net/URLLoader.hx
@@ -1,33 +1,73 @@
 package openfl.net;
 
+import sys.ssl.Socket;
 import openfl.events.Event;
 import openfl.events.IOErrorEvent;
+#if cpp
+import haxe.Http;
+#else
 import js.html.XMLHttpRequest;
+#end
 
 class URLLoader {
 	public var data:Dynamic;
 
+	#if cpp
+	private var xml:Http = null;
+	#else
 	private var xml = new XMLHttpRequest();
+	#end
 
 	public function new() {}
 
+	#if cpp
+	private dynamic function onloadend(e:Event) {}
+
+	private dynamic function onerror(e:Event) {}
+	#end
+
 	public function addEventListener(event:String, cb:Dynamic):Void {
+		#if cpp
+		switch (event) {
+			case Event.COMPLETE:
+				this.onloadend = cb;
+			case IOErrorEvent.IO_ERROR:
+				this.onerror = cb;
+		}
+		#else
 		switch (event) {
 			case Event.COMPLETE:
 				xml.onloadend = function() {
 					this.data = xml.responseText;
-					cb(new Event());
+					cb(new Event(Event.COMPLETE));
 				}
 			case IOErrorEvent.IO_ERROR:
 				xml.onerror = function() {
 					this.data = xml.responseText;
-					cb(new IOErrorEvent());
+					cb(new IOErrorEvent(IOErrorEvent.IO_ERROR));
 				}
 		}
+		#end
 	}
 
 	public function load(url:URLRequest):Void {
+		#if cpp
+		Socket.DEFAULT_VERIFY_CERT = false;
+		xml = new Http(url.url);
+		xml.onData = function(data) {
+			this.data = data;
+			onloadend(new Event(Event.COMPLETE));
+		};
+		xml.onError = function(data) {
+			this.data = data;
+			onerror(new IOErrorEvent(IOErrorEvent.IO_ERROR));
+		}
+		xml.setHeader("Content-Type", "application/json; charset=utf-8");
+		xml.setPostData(url.data);
+		xml.request(url.method == "POST");
+		#else
 		xml.open(url.method, url.url);
 		xml.send(url.data);
+		#end
 	}
 }
