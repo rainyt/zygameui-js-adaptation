15973240be9040fc1c957ea939f1547a649fce5f
yutian.liu <939029835@qq.com>
2021-06-02 12:40:47 +0800

    [IOS] setData getData新增NSUserDefaults的支持

--- JsSource/zygame/utils/Lib.hx
+++ JsSource/zygame/utils/Lib.hx
@@ -1,29 +1,38 @@
 package zygame.utils;
 
+import ios.foundation.NSUserDefaults;
+
 class Lib {
-    
-    public static function getData(key:String,defaultValue:Dynamic = null):Dynamic{
-        return defaultValue;
-    }
+	public static function getData(key:String, defaultValue:Dynamic = null):Dynamic {
+		#if objc
+		var value = NSUserDefaults.standardUserDefaults().stringForKey(key);
+		return value == null ? defaultValue : value.toString();
+		#else
+		return defaultValue;
+		#end
+	}
 
-    public static function setData(key:String,value:Dynamic):Void{
+	public static function setData(key:String, value:Dynamic):Void {
+		#if objc
+		NSUserDefaults.standardUserDefaults().setObjectForKey(value, key);
+		#else
+		return defaultValue;
+		#end
+	}
 
-    }
+	public static function getChannel():String {
+		return "cocos";
+	}
 
-    public static function getChannel():String {
-        return "cocos";
-    }
+	public static function getVersion():String {
+		return "1000";
+	}
 
-    public static function getVersion():String {
-        return "1000";
-    }
+	public static function setTimeout(cb:Dynamic, t:Int):Int {
+		return -1;
+	}
 
-    public static function setTimeout(cb:Dynamic,t:Int):Int{
-        return -1;
-    }
-
-    public static function getUUID():String{
-        return "####-####-####-###";
-    }
-
+	public static function getUUID():String {
+		return "####-####-####-###";
+	}
 }
